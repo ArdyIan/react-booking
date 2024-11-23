d98396248f29bd3f40dacecc0895813df18daf81
Ardy1an <adithardiyanto0@gmail.com>
2024-11-24 01:59:31 +0700

    property list

---
+++ src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,53 @@
+// import React from "react";
+
+// const Hotel = () => {
+//   return <div>hotel</div>;
+// };
+
+// export default Hotel;
+
+import "./propertyList.css";
+
+const PropertyList = () => {
+  return (
+    <div className="pList">
+      <div className="pListItem">
+        <img src="https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o=" alt="" className="pListImg" />
+        <div className="pListTitles">
+          <h1>Hotels</h1>
+          <h2>233 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img src="https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o=" alt="" className="pListImg" />
+        <div className="pListTitles">
+          <h1>Hotels</h1>
+          <h2>233 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img src="https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o=" alt="" className="pListImg" />
+        <div className="pListTitles">
+          <h1>Hotels</h1>
+          <h2>233 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img className="pListImg" src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o=" alt="" />
+        <div className="pListTitles">
+          <h1>Hotels</h1>
+          <h2>233 Hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img className="pListImg" src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o=" alt="" />
+        <div className="pListTitles">
+          <h1>Hotels</h1>
+          <h2>233 Hotels</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PropertyList;
