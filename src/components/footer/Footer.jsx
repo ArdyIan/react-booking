79eb4a1067a04711bcf1a3238836c36b3c93bc67
Ardy1an <adithardiyanto0@gmail.com>
2024-11-27 14:21:43 +0700

    footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,69 @@
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="fLists">
+        <ul className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Regoins</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Districts</li>
+          <li className="fListItem">Airport</li>
+          <li className="fListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Regoins</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Districts</li>
+          <li className="fListItem">Airport</li>
+          <li className="fListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Regoins</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Districts</li>
+          <li className="fListItem">Airport</li>
+          <li className="fListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Regoins</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Districts</li>
+          <li className="fListItem">Airport</li>
+          <li className="fListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Regoins</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Districts</li>
+          <li className="fListItem">Airport</li>
+          <li className="fListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Regoins</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Districts</li>
+          <li className="fListItem">Airport</li>
+          <li className="fListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Regoins</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Districts</li>
+          <li className="fListItem">Airport</li>
+          <li className="fListItem">Hotels</li>
+        </ul>
+      </div>
+      <div className="fText">&#169; Copyright 2024</div>
+    </div>
+  );
+};
+
+export default Footer;
